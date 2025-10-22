a675b3cf5a5718d3f8224cd6cd576e4cf8ab4205
James Penn <JamesLockyerPenn@gmail.com>
2025-10-23 07:34:39 +0800

    feat(init): Initial project creation.

---
+++ Systems/Managers/GameManager.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+using Perihelion.Types.Singletons;
+
+namespace Perihelion.Managers
+{
+    /// <summary> The main manager for the game scene. </summary>
+    public partial class GameManager : SingletonNode<GameManager>
+    {
+    }
+}
