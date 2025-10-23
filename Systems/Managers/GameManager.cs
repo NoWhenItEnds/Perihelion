9670b67cc14c449c88d88b115b346e799166ca25
James Penn <JamesLockyerPenn@gmail.com>
2025-10-23 21:38:32 +0800

    feat(mesh): Added normal planets.

--- Systems/Managers/GameManager.cs
+++ Systems/Managers/GameManager.cs
@@ -3,18 +3,35 @@ using System;
 using Perihelion.Types.Singletons;
 using Perihelion.Types.Debug;
 using Perihelion.Types.Extensions;
+using Perihelion.Mesh;
 
 namespace Perihelion.Managers
 {
     /// <summary> The main manager for the game scene. </summary>
     public partial class GameManager : SingletonNode<GameManager>
     {
+        /// <summary> How quickly time is moving. </summary>
+        /// <remarks> One in game day takes two hours. </remarks>
+        [ExportGroup("Settings")]
+        [Export] private Single _timescale = 12f;
+
+
+        /// <summary> The world's current time. </summary>
+        /// <remarks> The game starts on Sunday the 5th of February, 2012. </remarks>
+        public DateTimeOffset CurrentTime { get; private set; } = new DateTimeOffset(2012, 2, 5, 0, 0, 0, TimeSpan.FromHours(8));
+
+        /// <summary> The amount of time (in seconds) that has passed in game time since the previous physics frame. </summary>
+        public Double GameTimeDelta { get; private set; }
+
         [Export] private PackedScene _helper;
+
+        private CelestialObject[] _objects;
 
 
         /// <inheritdoc/>
         public override void _Ready()
         {
+            /*
             Random random = new Random();
 
             // Generate coordinates.
@@ -26,6 +43,35 @@ namespace Perihelion.Managers
                 AddChild(instance);
                 instance.GlobalPosition = MathExtensions.GeographicalToCartesian(coordinates[i].latitude, coordinates[i].longitude, 10);
                 instance.SetCustomText($"LAT: {coordinates[i].latitude:F2}, LON: {coordinates[i].longitude:F2}");
+            }
+            */
+
+            _objects = new CelestialObject[]
+            {
+                CelestialObject.MERCURY,
+                CelestialObject.VENUS,
+                CelestialObject.EARTH,
+                CelestialObject.MARS,
+                CelestialObject.JUPITER,
+                CelestialObject.SATURN,
+                CelestialObject.URANUS,
+                CelestialObject.NEPTUNE,
+                CelestialObject.PLUTO
+            };
+        }
+
+
+        /// <inheritdoc/>
+        public override void _PhysicsProcess(Double delta)
+        {
+            DateTimeOffset previousTime = CurrentTime;
+            CurrentTime = CurrentTime.AddSeconds(delta * _timescale);
+            GameTimeDelta = (CurrentTime.ToUnixTimeMilliseconds() - previousTime.ToUnixTimeMilliseconds()) * 0.001f;
+
+            foreach (CelestialObject obj in _objects)
+            {
+                Vector3 position = obj.CalculateCartesianPosition(CurrentTime);
+                DebugDraw3D.DrawSphere(position * 0.1f, 0.01f);
             }
         }
     }
--- Systems/Mesh/CelestialObject.cs
+++ Systems/Mesh/CelestialObject.cs
@@ -1,9 +1,9 @@
 using System;
+using Godot;
 
 namespace Perihelion.Mesh
 {
     /// <summary> A stellar object representing a location within space. </summary>
-    /// <remarks> https://github.com/mchrbn/unity-planetarium/blob/master/Assets/Scripts/CelestialCoordinates.cs </remarks>
     public class CelestialObject : IEquatable<CelestialObject>
     {
         /// <summary> The unique identifier or name of the object. </summary>
@@ -12,7 +12,7 @@ namespace Perihelion.Mesh
         /// <summary> The mean distance of the orbit in AU. 1 AU is the distance from the Sun to the Earth. </summary>
         public Double SemiMajorAxis { get; private set; }
 
-        /// <summary> The eccentricity of the orbit. Between 0.0 - 1.0, with 1.0 being perfectly circular. </summary>
+        /// <summary> The eccentricity of the orbit. Between 0.0 - 1.0, with 0.0 being perfectly circular. </summary>
         public Double Eccentricity { get; private set; }
 
         /// <summary> The angle of the orbit's tilt relative to the plane of the ecliptic. </summary>
@@ -49,33 +49,80 @@ namespace Perihelion.Mesh
         }
 
 
-        /*
-        public (Single X, Single Y, Single Z) CalculateCartesianPosition(DateTimeOffset time)
+        /// <summary> Calculate the current position of the object in 3D space given a time. </summary>
+        /// <param name="time"> The time to calculate the position at. </param>
+        /// <returns> The current position of the object in 3D space. </returns>
+        public Vector3 CalculateCartesianPosition(DateTimeOffset time)
         {
             DateTimeOffset j2000 = new DateTimeOffset(2000, 1, 1, 12, 0, 0, TimeSpan.Zero);
             Double julianDays = (time - j2000).TotalDays; // Calculate the number of days since 2000.
-            Double julianCenturies = julianDays / 36525.0f; // Calculate the number of centuries since 2000.
 
-            // Calculate mean anomaly.
-        }*/
+            Double meanAnomaly = (2 * Math.PI / OrbitalPeriod) * julianDays;
 
+            // Solve for eccentric anomaly using Newton-Raphson method.
+            Double eccentricAnomaly = meanAnomaly;
+            for (Int32 i = 0; i < 100; i++)
+            {
+                Double f = eccentricAnomaly - Eccentricity * Mathf.Sin(eccentricAnomaly) - meanAnomaly;
+                Double df = 1.0 - Eccentricity * Mathf.Cos(eccentricAnomaly);
+                eccentricAnomaly -= f / df;
+                if (Mathf.Abs(f) < 1e-10) { break; }
+            }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns></returns>
-        /// <remarks> https://www.princeton.edu/~willman/planetary_systems/Sol/ https://www.heavens-above.com/mars.aspx </remarks>
-        public static CelestialObject BuildMars()
-        {
-            return new CelestialObject("Mars", 1.52371, 0.09339, 105.9972, 49.57854, 336.04084, 686.497767);
+            // Calculate true anomaly
+            Double sqrtTerm = Math.Sqrt((1 + Eccentricity) / (1 - Eccentricity));
+            Double trueAnomaly = 2.0 * Mathf.Atan2(sqrtTerm * Mathf.Sin(eccentricAnomaly / 2.0), Mathf.Cos(eccentricAnomaly / 2.0));
+
+            // Calculate distance from the Sun
+            Double r = SemiMajorAxis * (1 - Eccentricity * Eccentricity) / (1 + Eccentricity * Mathf.Cos(trueAnomaly));
+
+            // Some values need to be in radians.
+            Double inclinationRad = Mathf.DegToRad(Inclination);
+            Double ascendingRad = Mathf.DegToRad(Ascending);
+            Double perihelionRad = Mathf.DegToRad(Perihelion);
+            Double arg = perihelionRad + trueAnomaly;
+
+            Double x = r * (Mathf.Cos(ascendingRad) * Mathf.Cos(arg) - Mathf.Sin(ascendingRad) * Mathf.Sin(arg) * Mathf.Cos(inclinationRad));
+            Double y = r * (Mathf.Sin(ascendingRad) * Mathf.Cos(arg) + Mathf.Cos(ascendingRad) * Mathf.Sin(arg) * Mathf.Cos(inclinationRad));
+            Double z = r * Mathf.Sin(arg) * Mathf.Sin(inclinationRad);
+
+            return new Vector3((Single)x, (Single)y, (Single)z);
         }
 
 
         /// <inheritdoc/>
-        public Boolean Equals(CelestialObject? other) => Id == other?.Id;
+        public Boolean Equals(CelestialObject? other) => Id.ToUpper() == other?.Id.ToUpper();
 
 
         /// <inheritdoc/>
-        public override Int32 GetHashCode() => HashCode.Combine(Id);
+        public override Int32 GetHashCode() => HashCode.Combine(Id.ToUpper());
+
+
+        /// <summary> Construct the planet Mercury. </summary>
+        public static CelestialObject MERCURY => new CelestialObject("Mercury", 0.3870993, 0.20564, 7.0, 75.05, 48.33, 87.9090455);
+
+        /// <summary> Construct the planet Venus. </summary>
+        public static CelestialObject VENUS => new CelestialObject("Venus", 0.723336, 0.00678, 3.39, 76.64, 75.47, 224.5469999);
+
+        /// <summary> Construct the planet Earth. </summary>
+        public static CelestialObject EARTH => new CelestialObject("Earth", 1.0, 0.01671, 0, 114.21, 102.94, 365.006351);
+
+        /// <summary> Construct the planet Mars. </summary>
+        public static CelestialObject MARS => new CelestialObject("Mars", 1.52371, 0.09339, 1.85, 49.56, 336.04, 686.497767);
+
+        /// <summary> Construct the planet Jupiter. </summary>
+        public static CelestialObject JUPITER => new CelestialObject("Jupiter", 5.2029, 0.0484, 1.3, 100.49, 273.66, 4329.854475);
+
+        /// <summary> Construct the planet Saturn. </summary>
+        public static CelestialObject SATURN => new CelestialObject("Saturn", 9.537, 0.0539, 2.49, 113.25, 339.42, 10748.33677);
+
+        /// <summary> Construct the planet Uranus. </summary>
+        public static CelestialObject URANUS => new CelestialObject("Uranus", 19.189, 0.04726, 0.77, 73.14, 312.01, 30666.14879);
+
+        /// <summary> Construct the planet Neptune. </summary>
+        public static CelestialObject NEPTUNE => new CelestialObject("Neptune", 30.0699, 0.00859, 1.77, 131.78, 280.87, 60148.8318);
+
+        /// <summary> Construct the planet Pluto. </summary>
+        public static CelestialObject PLUTO => new CelestialObject("Pluto", 39.4821, 0.24883, 17.14, 110.3, 113.75, 90527.592);
     }
 }
